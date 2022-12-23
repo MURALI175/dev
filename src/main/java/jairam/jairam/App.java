a64dcc13f85d8e40b97248b4aee22bf6e70d8097
MURALI175 <muralikrishnapenta@1998.gmail.com>
2022-12-23 12:33:51 +0530

    my 1st commit

---
+++ src/main/java/jairam/jairam/App.java
@@ -0,0 +1,13 @@
+package jairam.jairam;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
