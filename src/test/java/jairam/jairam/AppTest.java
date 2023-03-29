19c5d7f2387c25e7bc3b07a29eb2bd999d21c5c6
MURALI175 <121224706+MURALI175@users.noreply.github.com>
2023-03-29 11:43:58 +0530

    API

--- src/test/java/jairam/jairam/AppTest.java
+++ src/test/java/jairam/jairam/AppTest.java
@@ -1,20 +1,24 @@
-package jairam.jairam;
+pm.test("Verify latitude and longitude", function () {
+    var jsonData = pm.response.json();
+    pm.expect(jsonData.coord.lat).to.eql(51.51);
+    pm.expect(jsonData.coord.lon).to.eql(-0.13);
+});
 
-import static org.junit.Assert.assertTrue;
+// Test Case 1: Verify that the response has a 200 status code
+pm.test("Verify status code is 200", function () {
+    pm.response.to.have.status(200);
+});
 
-import org.junit.Test;
+// Test Case 2: Verify that the temperature is returned in Kelvin
+pm.test("Verify temperature is in Kelvin", function () {
+    var jsonData = pm.response.json();
+    pm.expect(jsonData.main.temp).to.be.a("number");
+    pm.expect(jsonData.main.temp).to.be.above(0);
+});
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
-}
+// Test Case 3: Verify that the weather description is returned
+pm.test("Verify weather description is returned", function () {
+    var jsonData = pm.response.json();
+    pm.expect(jsonData.weather[0].description).to.be.a("string");
+    pm.expect(jsonData.weather[0].description).to.not.be.empty;
+});
